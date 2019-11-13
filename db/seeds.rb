454e1ef19b876409e025c1b33093bd9f7c3567f4
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2019-11-13 13:52:41 -0300

    restaurants_controller up n running

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'detroying previous data...'
+
+Restaurant.destroy_all
+
+puts 'Data destroyed'
+
+rest_attr = [{
+  name: 'Vintão',
+  address: 'Endereço do Vintão',
+  phone_number: '3248743',
+  category: 'italian'
+}, {
+  name: 'Jacobina',
+  address: 'R. Alm. Tamandaré',
+  phone_number: '3248743',
+  category: 'japanese'
+}, {
+  name: 'Rause',
+  address: 'Al. Dr. Carlos de Carvalho',
+  phone_number: '3248743',
+  category: 'belgian'
+}, {
+  name: 'Babilônia',
+  address: 'Com. Araújo',
+  phone_number: '3248743',
+  category: 'french'
+}]
+
+puts 'creating seed data...'
+
+Restaurant.create(rest_attr)
+
+puts "#{Restaurant.count} restaurants created sucessfully"
